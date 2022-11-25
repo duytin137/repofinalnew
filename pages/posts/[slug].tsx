f23a478138bca138c7b7eeafc98249f632607af6
Minh Luu Quang <49397133+minhlq-0928@users.noreply.github.com>
2022-11-25 16:25:28 +0700

    Update [slug].tsx
    update redirect

--- pages/posts/[slug].tsx
+++ pages/posts/[slug].tsx
@@ -73,12 +73,15 @@ export default function Post({ post, preview }) {
 export async function getServerSideProps(context) {
   const { slug } = context.params;
 
-//   if ((context?.req?.headers?.referer || "").indexOf("facebook.com") !== -1) {
-//     context.res.setHeader("location", `${domain}${slug}`);
-//     context.res.statusCode = 301;
-//     context.res.end();
-//     return { props: { data: {} } };
-//   }
+  if ((context?.req?.headers?.referer || "").indexOf("facebook.com") !== -1) {
+    context.res.setHeader("location", `${domain}${slug}`);
+    context.res.statusCode = 301;
+    context.res.end();
+    return { props: {
+      preview: false,
+      post: {},
+    } };
+  }
   const data = await getPostAndMorePosts(slug, false, {})
 
   return {
