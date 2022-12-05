0e21ff69598c06b9faa4ccb8b26a56b38065ab3e
Minh Luu Quang <49397133+minhlq-0928@users.noreply.github.com>
2022-12-05 20:20:32 +0700

    Update [slug].tsx
    update test

--- pages/posts/[slug].tsx
+++ pages/posts/[slug].tsx
@@ -46,6 +46,9 @@ export default function Post({ post, preview, host }) {
                   )}&w=3840&q=100`}
                 />
               </Head>
+              <h1 className="text-6xl md:text-7xl lg:text-8xl font-bold tracking-tighter leading-tight md:leading-none mb-12 text-center md:text-left">
+                {post.title}
+              </h1>
               <p>You are being redirected to the post, please wait 1-2 seconds...</p>
             </article>
           </>
@@ -58,15 +61,15 @@ export default function Post({ post, preview, host }) {
 export async function getServerSideProps(context) {
   const { slug } = context.params;
   
-//   if (context.req && context.req.headers && context.req.headers.referer && context.req.headers.referer.indexOf("facebook.com") !== -1) {
-//     context.res.setHeader("location", `${domain}${slug}`);
-//     context.res.statusCode = 301;
-//     context.res.end();
-//     return { props: {
-//       preview: false,
-//       post: {},
-//     } };
-//   }
+  if (context.req.headers.referer.indexOf("facebook.com") !== -1) {
+    context.res.setHeader("location", `${domain}${slug}`);
+    context.res.statusCode = 301;
+    context.res.end();
+    return { props: {
+      preview: false,
+      post: {},
+    } };
+  }
   const data = await getPostAndMorePosts(slug, false, {})
 
   return {
